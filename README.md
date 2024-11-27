47e0e084dc4a18ae8708e4b0c786af6791064d47
GConscious <amrithgandham@gmail.com>
2024-11-26 17:16:36 -0800

    Setup pyterrier

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Implementation of search and recommendation algorithm(s) of the site: https://www.geeksforgeeks.org/
 
 pip install Flask
+pip install Pandas
+pip install python-terrier
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import pandas as pd 
+import pyterrier as pt 
 
 app = Flask(__name__)
 @app.route("/search")
-def search():
-    return("search system")
 
+def search():
+    #Change to your JDK path
+    pt.java.set_java_home("C:\\Users\\Amrith\\.jdks\\temurin-11.0.16.1")
+    if not pt.started():
+        pt.java.init()
+    return str(pt.java.started())
+  
+  
 if __name__ == "__main__":
     app.run(debug=True)
