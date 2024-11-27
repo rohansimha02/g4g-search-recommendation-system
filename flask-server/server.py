d2a956d8a2fad31a333d63689d07ae53e0153b81
vvaibava <65467493+vvaibava@users.noreply.github.com>
2024-11-26 18:59:27 -0800

    Changes to server.py

--- flask-server/server.py
+++ flask-server/server.py
@@ -11,7 +11,21 @@ def search():
     if not pt.started():
         pt.java.init()
     return str(pt.java.started())
-  
-  
+
+def index(): 
+    files = pt.io.find_files("/data")
+    indexer = pt.FilesIndexer("/data/g4g_index",
+                            meta={"docno":20,"filename":1024,"title":1024},meta_tags={"title":"title"})
+    indexref = indexer.index(files)
+    index = pt.IndexFactory.of(indexref)
+    
+def model():
+    queries = pd.DataFrame([["q1","python"], ["q2","nlp"]], columns=["qid","sql"])
+    index = pt.IndexFactory.of("./g4g_index_index/data.properties")
+    bm_25 = pt.BatchRetrieve(index, wmodel="BM_25")
+    bm_25.transform(queries)
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
