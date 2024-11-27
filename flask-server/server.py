0d707fcf12f2bfc5dade02c1f2b161a2e46f1edd
GConscious <amrithgandham@gmail.com>
2024-11-27 12:49:33 -0800

    Create search function

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,30 +1,11 @@
 from flask import Flask
-import pandas as pd 
-import pyterrier as pt 
 
 app = Flask(__name__)
 @app.route("/search")
 
 def search():
-    #Change to your JDK path
-    pt.java.set_java_home("C:\\Users\\Amrith\\.jdks\\temurin-11.0.16.1")
-    if not pt.started():
-        pt.java.init()
-    return str(pt.java.started())
-
-def index(): 
-    files = pt.io.find_files("/data")
-    indexer = pt.FilesIndexer("/data/g4g_index",
-                            meta={"docno":20,"filename":1024,"title":1024},meta_tags={"title":"title"})
-    indexref = indexer.index(files)
-    index = pt.IndexFactory.of(indexref)
-    
-def model():
-    queries = pd.DataFrame([["q1","python"], ["q2","nlp"]], columns=["qid","sql"])
-    index = pt.IndexFactory.of("./g4g_index_index/data.properties")
-    bm_25 = pt.BatchRetrieve(index, wmodel="BM_25")
-    bm_25.transform(queries)
-
+    #Search functionality here
+    return "hello"
 
 
 if __name__ == "__main__":
